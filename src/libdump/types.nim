137a89812174fb7c9c4e7fdb9e3d066ebda01097
Jake Leahy <jake@leahy.dev>
2026-03-18 20:17:26 +1100

    Add a field mapping helper
    This makes transformations between types a lot cleaner

---
+++ src/libdump/types.nim
@@ -0,0 +1,44 @@
+## Utilities for working with types
+
+import ./macros
+
+import std/options
+
+macro callForEachField(inp: typedesc, fun: untyped): typedesc =
+  ## Calls `func` on each field creating a new object
+  let decl = inp.getObjectDecl()
+  if decl.isNone:
+    echo inp.treeRepr
+    "Can't find declaration".error(inp)
+
+  proc rec(node: NimNode): NimNode =
+    ## Recursive function to rebuild a type but with different fields
+    case node.kind
+    of nnkIdentDefs:
+      return nnkIdentDefs.newTree(node[0], newCall(ident"mapperTmpl", node[1]), node[2])
+    else:
+      if node.canHaveSons:
+        result = node.kind.newTree()
+        for son in node:
+          result &= rec(son)
+      else:
+        return node
+  result = rec(decl.get())
+
+template transformFields*(inputObj: typedesc, mapper: untyped): typedesc =
+  ## Transforms the fields in an object to create a new type.
+  ## `mapper` is passed an implicit "inp" variable which contains the type
+  runnableExamples:
+    type Foo = object
+      a: string
+      b: int
+
+    type Transformed = transformFields(Foo, seq[inp])
+
+    echo Transformed(a: @["hello"], b: @[1])
+
+  # Inner template to have some kind of "callable"
+  template mapperTmpl(inp {.inject.}: untyped): typedesc =
+    mapper
+
+  callForEachField(inputObj, mapperTmpl)
